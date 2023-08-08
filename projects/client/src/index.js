82e23453145c5e615d06423229a0f9783970b465
usaid AKA <ujedkemal@gmail.com>
2023-08-08 13:27:38 +0700

    configuring folder and file structure

--- projects/client/src/index.js
+++ projects/client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./pages/App";
 import reportWebVitals from "./reportWebVitals";
-import { store } from "./store/store";
+import { store } from "./app/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- projects/client/src/pages/App.js
+++ projects/client/src/pages/App.js
@@ -1,7 +1,4 @@
-import axios from "axios";
-import logo from "./logo.svg";
 import "./App.css";
-import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {

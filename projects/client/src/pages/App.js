7b9ebef6bda471f0860722619a6742407249f193
usaid AKA <ujedkemal@gmail.com>
2023-08-08 13:22:28 +0700

    configuring folder and file structure

---
+++ projects/client/src/pages/App.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;

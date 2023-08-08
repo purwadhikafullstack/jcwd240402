d0f8061705004d0338405aa6a4b4b5b77b852a22
usaid AKA <ujedkemal@gmail.com>
2023-08-08 13:54:20 +0700

    configuring folder and file structure

--- projects/client/src/components/NavbarDesktop.jsx
+++ projects/client/src/components/NavbarDesktop.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import logo from "../assets/images/image_example.jpg";
 
 const NavbarDesktop = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="flex border-2 w-full justify-evenly items-center">
+        <div className="">
+          <img src={logo} alt="" className="w-14" />
+        </div>
+        <div className="flex w-60 border-2 justify-between">
+          <h1>Category</h1>
+          <h1>Inspiration</h1>
+        </div>
+        <div className="border-2">
+          <input type="text" />
+          <button>
+            <box-icon name="search-alt"></box-icon>
+          </button>
+        </div>
+        <div>
+          <box-icon type="solid" name="cart"></box-icon>
+          <box-icon type="solid" name="bell"></box-icon>
+          <button>Log in</button>
+          <button>Sign up</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavbarDesktop;
--- projects/client/src/pages/App.js
+++ projects/client/src/pages/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavbarDesktop from "../components/NavbarDesktop";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <NavbarDesktop />
         <Routes>
           <Route />
         </Routes>

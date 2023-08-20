e2128bbc3db7eb4a067db3ba472007cd0557a020
ncdncd <arka.socky@gmail.com>
2023-08-21 05:37:31 +0700

    storejs from admin/profiling

--- projects/client/src/app/store.js
+++ projects/client/src/app/store.js
@@ -1,13 +1,12 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import { adminReducer } from '../features/adminReducer';
-import { warehouseReducer } from '../features/warehouseReducer';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counterSlice";
+import userDataReducer from "../features/userDataSlice";
+import userAddressReducer from "../features/userAddressSlice";
 
-const rootReducer = combineReducers({
-    admin: adminReducer,
-    warehouse:warehouseReducer
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+    profiler: userDataReducer,
+    addresser: userAddressReducer,
+  },
 });
-
-export const store = createStore(rootReducer, applyMiddleware(thunk));
-
-

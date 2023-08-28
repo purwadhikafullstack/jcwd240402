67a3618047c0be2aeb3f3c15579c78de0934df5e
usaid AKA <ujedkemal@gmail.com>
2023-08-28 10:34:01 +0700

    commit admin/product

--- projects/client/src/pages/App.jsx
+++ projects/client/src/pages/App.jsx
@@ -1,9 +1,5 @@
 import "./App.css";
-<<<<<<< HEAD
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-=======
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
->>>>>>> 2afc9de9afca00ff7751b7e55d8f5be4b3a69ae3
 import AdminHome from "../pages/admin/AdminHome";
 import AdminLoginPage from "../pages/admin/AdminLogin";
 import AdminList from "../pages/admin/AdminList";
@@ -19,39 +15,21 @@ import ResetPassword from "./user/ResetPassword";
 import NotifResetPassword from "./user/NotifResetPassword";
 import NotFound from "./user/NotFound";
 import NotAuth from "./user/NotAuth";
-<<<<<<< HEAD
 import SettingProfile from "./user/SettingProfile";
 import SettingAddress from "./user/SettingAddress";
 import ProductDetail from "./user/ProductDetail";
-// import AdminProductDetail from "./admin/AdminList";
 import Cart from "./user/Cart";
 import CheckOut from "./user/CheckOut";
-import ProductRegister from "../components/Product/ProductRegister";
-import ProductList from "../components/Product/ProductList";
+import ProductRegister from "../components/admin/Product/ProductRegister";
+import ProductList from "../components/admin/Product/ProductList";
 import AdminProductPage from "./admin/AdminProductPage";
-import ProductEdit from "../components/Product/ProductEdit";
-import CategoryList from "./admin/CategoryList";
-import AdminStockPage from "./admin/AdminStockPage";
-import StockList from "../components/stock/StockList";
-import CreateStock from "../components/stock/CreateStock";
-import ProductPerCategory from "./user/ProductPerCategory";
-import Category from "./user/Category";
-=======
-import ProductRegister from "../components/admin/product/ProductRegister";
-import ProductList from "../components/admin/product/ProductList";
-import AdminProductPage from "./admin/AdminProductPage";
-import ProductEdit from "../components/admin/product/ProductEdit"
+import ProductEdit from "../components/admin/Product/ProductEdit";
 import CategoryList from "./admin/CategoryList";
 import AdminStockPage from "./admin/AdminStockPage";
 import StockList from "../components/admin/stock/StockList";
 import CreateStock from "../components/admin/stock/CreateStock";
-import SettingProfile from "./user/SettingProfile";
-import SettingAddress from "./user/SettingAddress";
-import ProductDetail from "./user/ProductDetail";
-
-import Cart from "./user/Cart";
-import CheckOut from "./user/CheckOut";
->>>>>>> 2afc9de9afca00ff7751b7e55d8f5be4b3a69ae3
+import ProductPerCategory from "./user/ProductPerCategory";
+import Category from "./user/Category";
 
 function App() {
   return (
@@ -78,17 +56,7 @@ function App() {
             element={<NotifResetPassword />}
           />
           <Route path="/forgot-password" element={<NotifForgotPassword />} />
-<<<<<<< HEAD
           <Route path="/category" element={<CategoryList />} /> {/* admin */}
-=======
-          <Route path="/user/setting" element={<SettingProfile />} />
-          <Route path="/user/setting/address" element={<SettingAddress />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<CheckOut />} />
-          <Route path="/category" element={<CategoryList/>} />
-
->>>>>>> 2afc9de9afca00ff7751b7e55d8f5be4b3a69ae3
           <Route path="/admin/products/*" element={<AdminProductPage />}>
             <Route index element={<ProductList />} />
             <Route path="create" element={<ProductRegister />} />{" "}
@@ -100,7 +68,6 @@ function App() {
             <Route index element={<StockList />} />
             <Route path="management" element={<CreateStock />} />
             <Route path="edit/:productName" element={<ProductEdit />} />
-<<<<<<< HEAD
           </Route>{" "}
           {/* admin dan pathnya di perbaiki*/}
           {/* 404 */}
@@ -130,13 +97,6 @@ function App() {
             element={<ProductPerCategory />}
           />
           <Route path="/product-category" element={<Category />} />
-=======
-          </Route>
-
-
-
-          <Route path="/*" element={<NotFound />} />
->>>>>>> 2afc9de9afca00ff7751b7e55d8f5be4b3a69ae3
         </Routes>
       </Router>
     </div>

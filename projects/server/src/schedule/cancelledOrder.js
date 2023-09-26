790b181b20d5fcd94755370d86942f03f4067614
ddnech <ddnech@gmail.com>
2023-09-26 20:14:18 +0700

    active node scheduler revised

--- projects/server/src/schedule/cancelledOrder.js
+++ projects/server/src/schedule/cancelledOrder.js
@@ -1,47 +1,41 @@
-const schedule = require("node-schedule");
-const dayjs = require("dayjs");
-const db = require("../models");
-
-const job = schedule.scheduleJob("*/15 * * *", async () => {
+const job = schedule.scheduleJob("*/15 * * * *", async () => {
   try {
-    const oneMinuteAgo = dayjs().subtract(1, "minute").toDate();
+    const sevenDaysAgo = dayjs().subtract(7, "day").toDate();
 
     const unpaidOrders = await db.Order.findAll({
       where: {
         [db.Sequelize.Op.or]: [{ order_status_id: 1 }, { order_status_id: 7 }],
         createdAt: {
-          [db.Sequelize.Op.lt]: oneMinuteAgo,
+          [db.Sequelize.Op.lt]: sevenDaysAgo,
         },
       },
     });
 
-    const t = await db.sequelize.transaction();
-
     for (let order of unpaidOrders) {
-      await db.Order.update(
-        { order_status_id: 5 },
-        { where: { id: order.id } },
-        { transaction: t }
-      );
+      const t = await db.sequelize.transaction();
+      try {
+        await db.Order.update(
+          { order_status_id: 5 },
+          { where: { id: order.id } },
+          { transaction: t }
+        );
 
-      await db.Reserved_stock.destroy({
-        where: { order_id: order.id },
-        include: [
-          {
-            model: db.Warehouse_stock,
-            as: "WarehouseProductReservation",
-          },
-        ],
-        transaction: t,
-      });
+        await db.Reserved_stock.destroy({
+          where: { order_id: order.id },
+          transaction: t,
+        });
+
+        await t.commit();
+      } catch (error) {
+        if (t && !t.finished) {
+          await t.rollback();
+        }
+        console.error("Error processing order:", order.id, error);
+      }
     }
 
-    await t.commit();
-    console.log(`Cancelled ${unpaidOrders.length} unpaid orders`);
+    console.log(`Cancelled ${unpaidOrders.length} unpaid orders older than 7 days`);
   } catch (error) {
-    if (t && !t.finished) {
-      await t.rollback();
-    }
     console.error("Error in the scheduled job:", error);
   }
 });

74b7564e6e076ce0ae46ffa8cec77d2496819454
High Call <49810289+haikal1223@users.noreply.github.com>
2023-10-01 13:54:52 +0700

    Update config.js

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -1,24 +1,24 @@
 require("dotenv").config({path: `../.env`});
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_DEV,
-    host: process.env.DB_HOST,
+    username: jcwd2442,
+    password: jcwd2442,
+    database: jcwd240402,
+    host: adminer2.purwadhikabootcamp.com,
     dialect: 'mysql',
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_TEST,
-    host: process.env.DB_HOST,
+    username: jcwd2442,
+    password: jcwd2442,
+    database: jcwd240402,
+    host: adminer2.purwadhikabootcamp.com,
     dialect: 'mysql',
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE_PROD,
-    host: process.env.DB_HOST,
+    username: jcwd2442,
+    password: jcwd2442,
+    database: jcwd240402,
+    host: adminer2.purwadhikabootcamp.com,
     dialect: 'mysql',
   },
 };

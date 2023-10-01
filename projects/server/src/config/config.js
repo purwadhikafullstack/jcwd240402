3a24111ea919fe138705ed874a96040805f1523d
High Call <49810289+haikal1223@users.noreply.github.com>
2023-10-01 14:06:44 +0700

    Update config.js

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -1,24 +1,24 @@
 require("dotenv").config({path: `../.env`});
 module.exports = {
   development: {
-    username: jcwd2442,
-    password: jcwd2442,
-    database: jcwd240402,
-    host: adminer2.purwadhikabootcamp.com,
+    username: 'jcwd2442',
+    password: 'jcwd2442',
+    database: 'jcwd240402',
+    host: 'adminer2.purwadhikabootcamp.com',
     dialect: 'mysql',
   },
   test: {
-    username: jcwd2442,
-    password: jcwd2442,
-    database: jcwd240402,
-    host: adminer2.purwadhikabootcamp.com,
+    username: 'jcwd2442',
+    password: 'jcwd2442',
+    database: 'jcwd240402',
+    host: 'adminer2.purwadhikabootcamp.com',
     dialect: 'mysql',
   },
   production: {
-    username: jcwd2442,
-    password: jcwd2442,
-    database: jcwd240402,
-    host: adminer2.purwadhikabootcamp.com,
+    username: 'jcwd2442',
+    password: 'jcwd2442',
+    database: 'jcwd240402',
+    host: 'adminer2.purwadhikabootcamp.com',
     dialect: 'mysql',
   },
 };
